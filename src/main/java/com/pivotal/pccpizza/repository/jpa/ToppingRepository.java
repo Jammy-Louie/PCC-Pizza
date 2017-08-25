2ee49218fe257ba87292671e4641576e9e83541c
Pivotal <sackett@nyc.pivotal.io>
2017-08-25 10:08:21 -0400

    Attempt to bind with PCC and use as a lookaside cache

--- src/main/java/com/pivotal/pccpizza/repository/jpa/ToppingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pivotal.pccpizza.repository.jpa;
-
-import com.pivotal.pccpizza.model.Topping;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ToppingRepository extends JpaRepository<Topping, Integer> {
-}
